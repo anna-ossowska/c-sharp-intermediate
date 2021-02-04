be807ad9e0fcbd83286ba8ba6a94336bda732fa7
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-04 18:15:20 +0100

    Constructors

---
+++ 01_classes/Constructors/Constructors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
