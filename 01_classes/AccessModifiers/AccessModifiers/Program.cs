14baad244034be0554727223d88a0f24984b930a
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-07 18:18:19 +0100

    Access Modifiers

---
+++ 01_classes/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        // Due to the private keyword we cannot access the _birthday field outside of the Person class
+        private DateTime _birthday;
+
+        // To set or get the birthday we need public methods
+        public void SetBirthday(DateTime birthday)
+        {
+            _birthday = birthday;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return _birthday;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthday(new DateTime(1995, 2, 16));
+            Console.WriteLine(person.GetBirthday());
+        }
+    }
+}
