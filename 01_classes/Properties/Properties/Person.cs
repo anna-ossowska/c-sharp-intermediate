672567c1fd678af9aaa9a3b1e67ea1f62e860ec3
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-08 12:42:09 +0100

    Properties

---
+++ 01_classes/Properties/Properties/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; private set; }
+
+        public Person(DateTime birthday)
+        {
+            Birthday = birthday;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthday;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+
+    }
+}
