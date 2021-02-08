d3f7dd4009de43f546e090d9b40b910a62a6df08
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-08 23:40:50 +0100

    Exercise 1 - Stopwatch

---
+++ 01_classes/Exc/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type an action: start | stop | exit");
+                var input = (Console.ReadLine()).ToLower();
+
+                if (input == "start")
+                {
+                    Stopwatch.Start();
+                    Console.WriteLine("The program has started...");
+                }
+                else if (input == "stop")
+                {
+                    var duration = Stopwatch.Stop();
+                    Console.WriteLine($"The program was stopped after: {duration} seconds.");
+                }
+                else if (input == "exit")
+                {
+                    Console.WriteLine("Closing the program...");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ 01_classes/Exc/Exercise1/Exercise1/Stopwatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Stopwatch
+    {
+        private static DateTime _startTime;
+        private static DateTime _stopTime;
+        private static bool _doAgain;
+
+        public static DateTime Start()
+        {
+            if (_doAgain == true)
+            {
+                throw new InvalidOperationException("You can start only once.");
+            }     
+            _doAgain = true;
+            return _startTime = DateTime.Now;
+        }
+
+        public static TimeSpan Stop()
+        {
+            if (_doAgain == false)
+            {
+                throw new InvalidOperationException("You can stop only once.");
+            }
+            _stopTime = DateTime.Now;
+            _doAgain = false;
+            return _stopTime - _startTime;
+        }
+    }
+}
+
+
+
+
