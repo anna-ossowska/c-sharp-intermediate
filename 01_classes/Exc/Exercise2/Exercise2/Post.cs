19a4b09b45f6e5511cdff1889eb5410685a76ec7
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-09 13:48:57 +0100

    Exercise 2 - StackOverflow Post

---
+++ 01_classes/Exc/Exercise2/Exercise2/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace Exercise2
+{
+    public class Post
+    {
+        public string Title {get; private set;}
+        public string Description {get; private set;}
+        public DateTime Date {get; private set;}
+        public int UpVotes { get; private set; }
+        public int DownVotes { get; private set; }
+        public int Count { get; private set; }
+
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Date = DateTime.Now;
+            this.UpVotes = 0;
+            this.DownVotes = 0;
+            this.Count = 0;
+
+        }
+
+        public string UpVote()
+        {
+            ++Count;
+            ++UpVotes;
+            return "You have upvoted the post.";
+
+        }
+
+        public string DownVote()
+        {
+            ++Count;
+            --DownVotes;
+            return "You have downvoted the post.";
+        }
+
+        public string Total()
+        {
+            return $"This post has {Count} votes.";
+
+        }
+    }
+}
---
+++ 01_classes/Exc/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post("OOP", "Quick intro to C# OOP");
+
+            Console.WriteLine(post1.Title);
+            Console.WriteLine(post1.Description);
+            Console.WriteLine(post1.Date);
+
+            Console.WriteLine(post1.UpVote());
+            Console.WriteLine(post1.UpVote());
+            Console.WriteLine(post1.DownVote());
+            Console.WriteLine(post1.UpVote());
+            Console.WriteLine(post1.UpVote());
+
+            Console.WriteLine(post1.Total());
+
+        }
+    }
+}
