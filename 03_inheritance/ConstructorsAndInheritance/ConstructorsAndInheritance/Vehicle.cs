29707a79c45cbfc74832f1b6aa47fb0de386b398
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-10 16:58:13 +0100

    Constructors and Inheritance

---
+++ 03_inheritance/ConstructorsAndInheritance/ConstructorsAndInheritance/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConstructorsAndInheritance
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            Console.WriteLine($"Vehicle is being initialized {registrationNumber}");
+        }
+    }
+}
