0ea80e6cacbbbd5f54c1dcf42f55dd67e9b48021
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-11 12:41:12 +0100

    Upcasting and Downcasting

---
+++ 03_inheritance/UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UpcastingDowncasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // text and shape are both references to the same object
+            // but they have different views (shape object does not have access to properties and methods specified inside the Text class)
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width); // 100
+
+
+            // Another example of upcasting:
+            // All the elements passed to the ArrayList are implicitly converted to the base Object class
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add("text");
+            list.Add(new Text());
+        }
+    }
+}
