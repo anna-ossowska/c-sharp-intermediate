c3f048467b3085cc55128521695861cc60b54eed
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-11 22:12:45 +0100

    Exercise 1 - Designing a Stack

---
+++ 03_inheritance/Exc/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push("hello");
+            stack.Push("bye");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.Clear();
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ 03_inheritance/Exc/Exercise1/Exercise1/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    public class Stack
+    {
+        private readonly List<object> list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                list.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException("Null objects are not accepted.");
+            }
+        }
+
+        public object Pop()
+        {
+            if (list.Count > 0)
+            {
+                var popped = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                return popped;
+            }
+            else
+            {
+                throw new InvalidOperationException("Operation cannot be performed on an empty list.");
+            }
+        }
+
+        public void Clear()
+        {
+            if (list.Count > 0)
+            {
+                list.Clear();
+            }
+            else
+            {
+                throw new InvalidOperationException("Operation cannot be performed on an empty list.");
+            }
+        }
+    }
+}
