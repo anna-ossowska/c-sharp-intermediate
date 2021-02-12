2148a7b8b616fdad5e2c700b47e31bbfbfc86d02
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-12 14:45:29 +0100

    Method Overriding

---
+++ MethodOverriding/MethodOverriding/Canvas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverwriting
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ MethodOverriding/MethodOverriding/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MethodOverwriting
+{
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle.");
+        }
+
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle.");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle.");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
