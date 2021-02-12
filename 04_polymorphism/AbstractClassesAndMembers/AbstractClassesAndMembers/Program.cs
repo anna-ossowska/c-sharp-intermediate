5f231bcbea0e83885e222e58824d4466c0e12bd6
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-12 16:47:49 +0100

    Abstract classes and members

---
+++ 04_polymorphism/AbstractClassesAndMembers/AbstractClassesAndMembers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AbstractClassesAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape is declared as abstract. Thus, we cannot create an instance of this class.
+            // var shape = new Shape();
+
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
---
+++ 04_polymorphism/AbstractClassesAndMembers/AbstractClassesAndMembers/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AbstractClassesAndMembers
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("Shape has been copied into clipboard.");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("Shape has been selcted");
+        }
+    }
+}
