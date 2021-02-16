ff2e297900d5d0431d7632b1504bf1b8aaf6e100
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-16 16:15:48 +0100

    Interfaces and Extensibility

---
+++ 05_interfaces/InterfacesAndExtensibility/InterfacesAndExtensibility/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InterfacesAndExtensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger(@"C:\Users\tomas\Desktop\my_files\folder1\notes.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
